71058902d6f6143c5faa5de4ad5a83c83f6c4319
김자연 <jykim.developer@gmail.com>
2024-03-19 20:32:03 +0900

    [frontend] Chore: Styled Components 전역 스타일링 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom"
+import GlobalStyles from "./styles/GlobalStyles"
 import TripRouteWrite from "./pages/Main/TripRouteWrite"
+import { ThemeProvider } from "styled-components"
+import theme from "./styles/theme"
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<TripRouteWrite />} />
-        <Route path="/route-write" element={<TripRouteWrite />} />
-      </Routes>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<TripRouteWrite />} />
+        </Routes>
+      </ThemeProvider>
     </>
   )
 }
---
+++ frontend/src/styles/StyledComponents.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+    min-height: 85vh;
+    padding-bottom: 100px;
+`;
+
+export const FixedButton = styled.button`
+width: 100%;
+position: fixed;
+bottom: 0;
+padding: 15px;
+background: #ffda23;
+color: black;
+font-size: 1rem;
+font-weight: 600;
+`;
